9465b1be0c2a9f4aae4e046802de73a5d1f074c3
Mukul Singhal <mukul@Mukuls-MacBook-Air.local>
2022-11-07 15:09:18 +0530

    ci: added new files and established end points

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.status(200).json('Welcome to Laundri API zombie endpoint');
+});
+
+const port = process.env.port || 3000;
+app.listen(port,()=>{
+    console.log(`Server Started at port ${port}`);
+});
