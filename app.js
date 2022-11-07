476a83a346c369b5477d82e2ba15a85c6fb4d4e2
Mukul  Singhal <79706982+mukulsinghal407@users.noreply.github.com>
2022-11-07 15:18:19 +0530

    changes in port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/',(req,res)=>{
     res.status(200).json('Welcome to Laundri API zombie endpoint');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log(`Server Started at port ${port}`);
 });
